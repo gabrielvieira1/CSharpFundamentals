b45175182fe50c0aac01e2fbedd31fd2509c0e41
Gabriel Vieira <gabrielvieira1@protonmail.com>
2020-12-05 21:46:09 -0300

    first commit

---
+++ CSharpFundamentals/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using CSharpFundamentals.Math;
+
+namespace CSharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var gabriel = new Person();
+            gabriel.FirstName = "Gabriel";
+            gabriel.LastName = "Vieira";
+            gabriel.Introduce();
+
+            Calculator calculator = new Calculator();
+            var result = calculator.Add(1, 2);
+            Console.WriteLine(result);
+        }
+    }
+}
